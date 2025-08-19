47e394a887958211f9db32358557f87f00f9aa24
Hacker-Dom <18601956+hacker-DOM@users.noreply.github.com>
2025-08-19 08:12:41 -0400

    M Feat: Add test_initialize().

--- test/TestBase.t.sol
+++ test/TestBase.t.sol
@@ -45,3 +45,57 @@ contract SparkVaultTestBase is Test {
     }
 
 }
+
+contract SparkVaultMiscellaneousTests is SparkVaultTestBase {
+        // asset  = asset_;
+        // name   = name_;
+        // symbol = symbol_;
+        //
+        // _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        //
+        // chi = uint192(RAY);
+        // rho = uint64(block.timestamp);
+        // ssr = RAY;
+        //
+        // minSsr = RAY;
+        // maxSsr = RAY;
+    // keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.Initializable")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant INITIALIZABLE_STORAGE = 0xf0c57e16840df040f15088dc2f81fe391c3923bec73e23a9662efc9c229c6a00;
+
+    function test_initialize() public {
+        // >> Don't use the vault created by `setUp()`, create our own here:
+        vault = SparkVault(
+            address(new ERC1967Proxy(
+                address(new SparkVault()),
+                ""
+            ))
+        );
+        // >> Assert that the vault is not initialized
+        assertEq(
+            vm.load(
+                address(vault),
+                INITIALIZABLE_STORAGE
+            ),
+            bytes32(0)
+        );
+
+        // >> Action
+        vault.initialize(
+            address(asset),
+            "Spark Savings USDC V2",
+            "spUSDC",
+            admin
+        );
+
+        // >> Assert that the vault has been initialized
+        assertEq(
+            vm.load(
+                address(vault),
+                INITIALIZABLE_STORAGE
+            ),
+            bytes32(uint256(1))
+        );
+    }
+
+}
+
